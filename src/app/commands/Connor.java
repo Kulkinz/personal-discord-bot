bb484b88118e832ae750b6e693ab6c92cdccd9cf
Kulkinz <cool@coolspages.com>
2020-03-15 15:45:18 -0600

    payback

--- src/app/commands/Connor.java
+++ src/app/commands/Connor.java
@@ -9,9 +9,17 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class Connor {
     public Connor(MessageReceivedEvent event) {
         MessageChannel channel = event.getChannel();
+        try {
+            event.getMessage()
+            .delete()        
+            .queueAfter(5, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            System.out.println("Could not delete");
+        }
         channel.sendMessage("<@393804816419389440>")
-        .delay(10, TimeUnit.MILLISECONDS)
+        .delay(5, TimeUnit.MILLISECONDS)
         .flatMap(Message::delete)
         .queue();
+        
     }
 }
